3783d54b3fe3219ea63ea2c45140f9b9175e48f0
Patel Rushi <59217165+rushipatel7@users.noreply.github.com>
2020-07-09 00:18:08 +0530

    Update twitter-scapping.py

--- twitter-scapping.py
+++ twitter-scapping.py
@@ -2,28 +2,25 @@ import tweepy
 import time
 import csv
 import pandas as pd
-consumer_key = "kDLqXCiWUxPdsiu5rTFu0Ektu"
-consumer_secret = "YaQVmi3jP4xNsjttWLBi4AGlhsduqlqYxM04ao3vIbLqbMlVQH"
-access_token = "1083760447604453376-gnnhJutuddhffgtIDy2sxR3q79biuX"
-access_token_secret = "5iqN6mrcQSBizSROcE3Lhtet1EJcklkgVT9ZeXRngQUBi"
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-tweets_text  = tweepy.Cursor(api.search,q="blacklivesmatter",count=50,lang='en',since="2017-04-03").items(50)
-# csvFile = open('tweets.csv', 'a')
-# csvWriter = csv.writer(csvFile)
+tweets_text  = tweepy.Cursor(api.search,q="blacklivesmatter -filter:retweets",result_type="popular",count=5,lang='en',tweet_mode='extended').items(5)
 
 tweets_dict ={}
 tweets_no = 0
+
 for tweet in tweets_text:
-    print (tweet.created_at, tweet.text)
-    
+    tweet.full_text = [x for x in tweet.full_text.split(' ')[:-1]]
+    tweet.full_text = ' '.join(tweet.full_text)
+    print (tweet.user.name,tweet.created_at, tweet.full_text)
     tweets_no += 1
-    tweets_dict[tweets_no] = [tweet.created_at, tweet.text]
-
+    tweets_dict[tweets_no] = [tweet.user.name,tweet.created_at, tweet.full_text]
 df = pd.DataFrame.from_dict(tweets_dict,orient='index',columns=['name','tweet'])
-df.to_csv('scrape-data')
-
-
-
+df.to_csv('tweet.csv',index=False,encoding='utf-8-sig')
